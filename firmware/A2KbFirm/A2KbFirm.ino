7b6d2670036436b7a7be6fb40ffb22b25af1abca
Ángel González <aglezabad@gmail.com>
2017-03-28 11:13:38 +0200

    Added configuration header

--- firmware/A2KbFirm/A2KbFirm.ino
+++ firmware/A2KbFirm/A2KbFirm.ino
@@ -1,8 +1,13 @@
 #include <HID-Project.h>
 #include <HID-Settings.h>
+#include "Configuration.h"
+
+const size_t rows_length = sizeof(rows)/sizeof(rows[0]);
 
 void setup() {
-
+  for (int i=0; i<rows_length; i++) {
+    pinMode(i, OUTPUT);
+  }
   NKROKeyboard.begin();
 }
 
---
+++ firmware/A2KbFirm/Configuration.h
@@ -0,0 +1,5 @@
+#ifndef CONFIGURATION_H_
+#define CONFIGURATION_H_
+const int rows[] = {2,3,4,5,6,7};
+
+#endif
