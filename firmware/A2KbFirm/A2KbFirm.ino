26c610cf87177d68b9b803f21f6f5ee24e95a363
Ángel González <aglezabad@gmail.com>
2017-03-29 17:00:20 +0200

    Checking if it\'s possible to use Spanish layout

--- firmware/A2KbFirm/A2KbFirm.ino
+++ firmware/A2KbFirm/A2KbFirm.ino
@@ -1,18 +1,21 @@
 #include <HID-Project.h>
-#include <HID-Settings.h>
+#include "HID-Settings.h"
 #include "Configuration.h"
-
-//Constants.
-const size_t pin_rows_length = sizeof(pin_rows)/sizeof(pin_rows[0]);
-const size_t analog_pin_cols_length = sizeof(analog_pin_cols)/sizeof(analog_pin_cols[0]);
 
 //Variables.
 int row = 0;
-unsigned char analog_values_byte[analog_pin_cols_length];
+unsigned char analog_values[analog_pin_cols_length];
 
+//Extra functions
+boolean isNthBitSet (unsigned char c, int n) {
+  static unsigned char mask[] = {1, 2, 4, 8, 16, 32, 64, 128};
+  return ((c & mask[n]) != 0);
+}
+
+//Arduino functions
 void setup() {
   //Setup row pins.
-  for (int i=0; i<pin_rows_length; i++) {
+  for (int i = 0; i < pin_rows_length; i++) {
     pinMode(i, OUTPUT);
   }
   NKROKeyboard.begin();
@@ -22,9 +25,20 @@ void loop() {
   //Loop; set HIGH to one row pin; read analog values A0, A1, A2; send NKRO chars; set LOW to row pin.
   row %= pin_rows_length;
   digitalWrite(pin_rows[row], HIGH);
-  for (int i=0; i<analog_pin_cols_length; i++) {
-    analog_values_byte[i] = analogRead(analog_pin_cols[i]) & 0xff;  
+  for (int i = 0; i < analog_pin_cols_length; i++) {
+    analog_values[i] = lowByte(analogRead(analog_pin_cols[i]));
+    for (int j = 0; j < 8; j++) {
+      if(isNthBitSet(analog_values[i], j)) {
+        
+      }
+    }
   }
+
+  NKROKeyboard.send();
+  NKROKeyboard.releaseAll();
+  delay(300);
+
   digitalWrite(pin_rows[row], LOW);
 }
+
 
--- firmware/A2KbFirm/Configuration.h
+++ firmware/A2KbFirm/Configuration.h
@@ -1,9 +1,13 @@
 #ifndef CONFIGURATION_H_
 #define CONFIGURATION_H_
+
 //Digital pins set to OUTPUT for rows.
 const int pin_rows[] = {2,3,4,5,6,7};
+const size_t pin_rows_length = sizeof(pin_rows)/sizeof(pin_rows[0]);
 
 //Analog pins (if used as analog inputs) for reading the columns.
 const int analog_pin_cols[] = {0,1,2};
+const size_t analog_pin_cols_length = sizeof(analog_pin_cols)/sizeof(analog_pin_cols[0]);
 
 #endif
+
