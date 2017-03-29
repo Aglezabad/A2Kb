71963ab78b5668415567e1f426bcd60e4f634a60
Ángel González <aglezabad@gmail.com>
2017-03-29 11:04:09 +0200

    Firmware: Doing main loop.

--- firmware/A2KbFirm/A2KbFirm.ino
+++ firmware/A2KbFirm/A2KbFirm.ino
@@ -2,21 +2,29 @@
 #include <HID-Settings.h>
 #include "Configuration.h"
 
+//Constants.
 const size_t pin_rows_length = sizeof(pin_rows)/sizeof(pin_rows[0]);
-const size_t analog_pin_cols_length = sizeof(analog_pin_cols)/sizeof(&analog_pin_cols[0]);
+const size_t analog_pin_cols_length = sizeof(analog_pin_cols)/sizeof(analog_pin_cols[0]);
+
+//Variables.
+int row = 0;
+unsigned char analog_values_byte[analog_pin_cols_length];
 
 void setup() {
   //Setup row pins.
   for (int i=0; i<pin_rows_length; i++) {
     pinMode(i, OUTPUT);
   }
-  //Setup ADC values variables (only one byte is used).
-  unsigned char analog_values_byte[analog_pin_cols_length];
   NKROKeyboard.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //Loop; set HIGH to one row pin; read analog values A0, A1, A2; send NKRO chars; set LOW to row pin.
+  row %= pin_rows_length;
+  digitalWrite(pin_rows[row], HIGH);
+  for (int i=0; i<analog_pin_cols_length; i++) {
+    analog_values_byte[i] = analogRead(analog_pin_cols[i]) & 0xff;  
+  }
+  digitalWrite(pin_rows[row], LOW);
 }
 
--- firmware/A2KbFirm/Configuration.h
+++ firmware/A2KbFirm/Configuration.h
@@ -4,6 +4,6 @@
 const int pin_rows[] = {2,3,4,5,6,7};
 
 //Analog pins (if used as analog inputs) for reading the columns.
-const char* analog_pin_cols[] = {"A0", "A1", "A2"};
+const int analog_pin_cols[] = {0,1,2};
 
 #endif
